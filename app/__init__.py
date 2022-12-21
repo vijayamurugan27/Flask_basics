b50320342fcf5e85f86cc9879d89e558922a043d
vijayamurugan27@gmail.com <vijayamurugan27@gmail.com>
2022-12-21 15:37:02 +0530

    Database
    We have updated the necessary codes for Using the database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
 
+# for suing database
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
+# for db
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -23,8 +23,15 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title = 'Sign In', form = form)
 
+
+
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db' : db, 'User': User, 'Post':Post }
