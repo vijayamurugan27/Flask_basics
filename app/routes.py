57005b13d45b8438fc67b45b6001e7b93d18cec5
vijayamurugan27 <vijayamurugan27@gmail.com>
2022-12-21 06:41:07 +0530

    Passing a user name
    This example shows how to pass a username in the flask application.

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,14 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<h1> Hello, World! </h1>"
-
+    # return "<h1> Hello, World! </h1>"
+    user = {'username': 'Miguel'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
